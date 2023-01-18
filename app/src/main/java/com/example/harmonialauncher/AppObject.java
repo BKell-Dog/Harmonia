2fa59ec41e1f4fe9f7befb4ca88ea6d1e312ffed
Michael <michaelhaluskajr@gmail.co>
2023-01-18 12:34:31 -0500

    deleted comment

--- app/src/main/java/com/example/harmonialauncher/AppObject.java
+++ app/src/main/java/com/example/harmonialauncher/AppObject.java
@@ -7,7 +7,7 @@ import androidx.appcompat.content.res.AppCompatResources;
 import com.example.harmonialauncher.lockManager.Lockable;
 
 public class AppObject implements Lockable {
-    private String name, packageName; //business stuff
+    private String name, packageName;
     private Drawable image = null;
     private Boolean isAppInDrawer;
     private int imageId;
